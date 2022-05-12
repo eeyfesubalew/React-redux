3de0fcfa5d99b8a2b95d9eab0e6262c3df8b099a
eeyfesubalew <esubalew.seife@gmail.com>
2022-05-11 16:51:40 -0800

    setting up redux store

---
+++ src/store/srore.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { createSrore } from "redux";
+
+const counterReducer = (state, action) => {
+  if (action.type === "increment") {
+    return {
+      counter: state.counter + 1,
+    };
+  }
+  if (action.type === "decrement") {
+    return {
+      counter: state.counter - 1,
+    };
+  }
+  return state;
+};
+const store = createStore(counterReducer);
